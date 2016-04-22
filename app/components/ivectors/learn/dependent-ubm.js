6e2ed8f2c0ba729d5ae51195e1874d6bc094438f
Maxime Robin <contact.waxo@gmail.com>
2016-04-22 09:54:00 +0200

    UBM dependent extraction done

--- app/components/ivectors/learn/dependent-ubm.js
+++ app/components/ivectors/learn/dependent-ubm.js
@@ -3,15 +3,11 @@ import SPro from '../../../ivectors/0_1_Prepare_PRM/Spro';
 const exec = require('child_process').exec;
 const BluebirdPromise = require('bluebird');
 const fs = BluebirdPromise.promisifyAll(require('fs-extra'));
-const rimraf = BluebirdPromise.promisify(require('rimraf'));
 
 
 const ivectorsPath = `${process.cwd()}/app/ivectors`;
 const prmPath = `${ivectorsPath}/prm`;
 const lblPath = `${ivectorsPath}/lbl`;
-// const gmmPath = `${ivectorsPath}/gmm`;
-const matrixPath = `${ivectorsPath}/mat`;
-const iv = `${ivectorsPath}/iv`;
 
 const dependentPath = `${ivectorsPath}/dependent`;
 
@@ -19,72 +15,61 @@ const preparePath = `${ivectorsPath}/0_1_Prepare_PRM`;
 const UBMPath = `${ivectorsPath}/0_2_UBM_TotalVariability`;
 const extractIVPath = `${ivectorsPath}/0_3_Extract_Classes`;
 
-let inputClassesUBM = [];
-
-const cleanAndRegenerateGMM = function () {
-  return new BluebirdPromise((resolve) => {
-    rimraf(dependentPath)
-      .then(() => fs.mkdirAsync(dependentPath))
-      .finally(() => resolve());
-  });
-};
-
-const cleanAndRegenerateTV = function () {
-  return new BluebirdPromise((resolve) => {
-    rimraf(`${ivectorsPath}/mat`)
-      .then(() => fs.mkdirAsync(`${ivectorsPath}/mat`))
-      .then(() => resolve());
-  });
-};
-
-const cleanAndRegenerateIV = function () {
-  return new BluebirdPromise((resolve) => {
-    rimraf(iv)
-      .then(() => fs.mkdirAsync(iv))
-      .then(() => fs.mkdirAsync(`${iv}/raw`))
-      .finally(() => resolve());
-  });
-};
+let inputClasses = [];
 
 const dependentLST = function () {
   return new BluebirdPromise(resolve => {
-    let writeFiles = [];
+    let fileWriter = [];
     let regWav = new RegExp('.wav', 'g');
-    fs.readdirAsync(`${preparePath}/input`)
-      .then(inputClasses => {
-        inputClassesUBM = inputClasses;
-        inputClasses.forEach(cluster => {
-          fs.readdirAsync(`${preparePath}/input/${cluster}`)
-            .then(files => {
-              files.forEach((file, index) => {
-                files[index] = `${cluster}/${file}`.replace(regWav, '');
-              });
-              files = files.join('\n');
-              writeFiles.push(fs.writeFileAsync(
-                `${dependentPath}/totalvariability_${cluster}.ndx`,
-                files));
-              writeFiles.push(
-                fs.writeFileAsync(`${dependentPath}/data_${cluster}.lst`,
-                  files));
-            });
-        });
-        setTimeout(() => {
-          BluebirdPromise.all(writeFiles)
-            .then(() => resolve());
-        }, 1000);
-      });
+    inputClasses.forEach(cluster => {
+      fs.readdirAsync(`${preparePath}/input/${cluster}`)
+        .then(files => {
+          files.forEach((file, index) => {
+            files[index] = `${cluster}/${file}`.replace(regWav, '');
+          });
+          files = files.join('\n');
+          fileWriter.concat([
+            fs.writeFileAsync(
+              `${dependentPath}/${cluster}/totalvariability.ndx`,
+              files),
+            fs.writeFileAsync(
+              `${dependentPath}/${cluster}/data.lst`, files)
+          ]);
+        })
+        .catch(err => console.log(err));
+    });
+
+    setTimeout(() => {
+      BluebirdPromise.all(fileWriter)
+        .then(() => {
+          resolve();
+        }).catch(err => console.error(err));
+    }, 1000);
   });
 };
 
 const dependentDir = function () {
   let dirs = [];
-  inputClassesUBM.forEach(cluster => {
-    dirs.push(fs.mkdirAsync(`${dependentPath}/${cluster}`));
+  return new BluebirdPromise(resolve => {
+    fs.readdirAsync(`${preparePath}/input`)
+      .then(clusters => {
+        inputClasses = clusters;
+        inputClasses.forEach(cluster => {
+          dirs.concat([
+            fs.mkdirsAsync(`${dependentPath}/${cluster}/gmm`),
+            fs.mkdirsAsync(`${dependentPath}/${cluster}/mat`),
+            fs.mkdirsAsync(`${dependentPath}/${cluster}/iv/raw`)
+          ]);
+        });
+        return BluebirdPromise.all(dirs);
+      })
+      .then(() => {
+        setTimeout(() => resolve(), 1000);
+      });
   });
-  return BluebirdPromise.all(dirs);
 };
 
-const execAsync = function (execute) {
+const execAsync = function (execute, stdoutON) {
   return new BluebirdPromise(resolve => {
     exec(execute, (error, stdout, stderr) => {
       if (stderr) {
@@ -93,7 +78,9 @@ const execAsync = function (execute) {
       if (error !== null) {
         console.log(`exec error: ${error}`);
       }
-      console.log(`stdout: ${stdout}`);
+      if (stdoutON) {
+        console.log(`stdout: ${stdout}`);
+      }
 
       resolve();
     });
@@ -128,6 +115,7 @@ export default Ember.Component.extend({
     prepareFiles() {
       console.log('Prepare');
       this.set('isPrepareProcess', true);
+
       var spro = SPro.create({
         path: ivectorsPath,
         specificPath: preparePath,
@@ -140,13 +128,14 @@ export default Ember.Component.extend({
       spro.addObserver('isDone', () => {
         this.set('isPrepareProcess', !spro.get('isDone'));
         this.set('isPrepareDone', spro.get('isDone'));
-        setTimeout(() => this.send('normEnergy'), 5);
+        setTimeout(() => this.send('normEnergy'), 50);
       });
     },
 
     normEnergy() {
       console.log('Norm Energy');
       this.set('isEnergyProcess', true);
+
       let command = `${preparePath}/NormFeat`;
       let options = [
         `--config ${preparePath}/cfg/NormFeat_energy.cfg`,
@@ -165,13 +154,14 @@ export default Ember.Component.extend({
 
         this.set('isEnergyProcess', false);
         this.set('isEnergyDone', true);
-        this.send('normFeatures');
+        setTimeout(() => this.send('normFeatures'), 50);
       });
     },
 
     normFeatures() {
       console.log('Norm Features');
       this.set('isFeaturesProcess', true);
+
       let command = `${preparePath}/NormFeat`;
       let options = [
         `--config ${preparePath}/cfg/NormFeat.cfg`,
@@ -197,135 +187,131 @@ export default Ember.Component.extend({
 
     trainUBM() {
       console.log('Train UBM');
-      cleanAndRegenerateGMM()
-        .then(() => dependentLST())
+      this.set('isUBMProcess', true);
+
+      fs.removeAsync(dependentPath)
         .then(() => dependentDir())
+        .then(() => dependentLST())
         .then(() => {
-          this.set('isUBMProcess', true);
+          let executeUBM = [];
           let command = `${UBMPath}/TrainWorld`;
 
-          let executeUBM = [];
-          let createGMMDirs = [];
-
-          inputClassesUBM.forEach(cluster => {
-            let dirGMM = `${dependentPath}/${cluster}/gmm`;
+          inputClasses.forEach(cluster => {
             let options = [
               `--config ${UBMPath}/cfg/TrainWorld.cfg`,
-              `--inputFeatureFilename ${dependentPath}/data_${cluster}.lst`,
+              `--inputFeatureFilename ${dependentPath}/${cluster}/data.lst`,
               `--featureFilesPath ${prmPath}/`,
-              `--labelFilesPath ${lblPath}`,
-              `--mixtureFilesPath ${dirGMM}/`
+              `--labelFilesPath ${lblPath}/`,
+              `--mixtureFilesPath ${dependentPath}/${cluster}/gmm/`
             ];
-
             let execute = `${command} ${options.join(' ')}`;
-            createGMMDirs.push(fs.mkdirAsync(dirGMM));
             executeUBM.push(execAsync(execute));
           });
-
-          BluebirdPromise.all(createGMMDirs)
-            .then(() => BluebirdPromise.all(executeUBM))
+          BluebirdPromise.all(executeUBM)
             .then(() => {
               this.set('isUBMProcess', false);
               this.set('isUBMDone', true);
-              // this.send('trainTotalVariability');
+              this.send('trainTotalVariability');
             });
         });
     },
 
     trainTotalVariability() {
       console.log('Train TV');
-      fs.createReadStream(`${ivectorsPath}/data.lst`)
-        .pipe(fs.createWriteStream(`${UBMPath}/totalvariability.ndx`));
-      cleanAndRegenerateTV().then(() => {
-        this.set('isTVProcess', true);
-        let command = `${UBMPath}/TotalVariability`;
+      this.set('isTVProcess', true);
+
+      let executeTV = [];
+      let command = `${UBMPath}/TotalVariability`;
+
+      inputClasses.forEach(cluster => {
         let options = [
           `--config ${UBMPath}/cfg/TotalVariability_fast.cfg`,
           `--featureFilesPath ${prmPath}/`,
           `--labelFilesPath ${lblPath}/`,
-          `--mixtureFilesPath ${gmmPath}/`,
-          `--matrixFilesPath ${ivectorsPath}/mat/`,
-          `--ndxFilename ${UBMPath}/totalvariability.ndx`
+          `--mixtureFilesPath ${dependentPath}/${cluster}/gmm/`,
+          `--matrixFilesPath ${dependentPath}/${cluster}/mat/`,
+          `--ndxFilename ${dependentPath}/${cluster}/totalvariability.ndx`
         ];
 
         let execute = `${command} ${options.join(' ')}`;
-        exec(execute, (error, stdout, stderr) => {
-          if (stderr) {
-            console.log(`stderr: ${stderr}`);
-          }
-          if (error !== null) {
-            console.log(`exec error: ${error}`);
-          }
+        executeTV.push(execAsync(execute));
+      });
+
+      BluebirdPromise.all(executeTV)
+        .then(() => {
           this.set('isTVProcess', false);
           this.set('isTVDone', true);
           this.send('prepareIVectorsExtractor');
         });
-      });
     },
 
     prepareIVectorsExtractor() {
       console.log('Prepare IV');
       this.set('isIVectorsProcess', true);
-      fs.readFileAsync(`${ivectorsPath}/data.lst`)
-        .then(data => {
-          data = data.toString().split('\n');
-          let ndx = '';
-          let plda = '';
-          let trainModel = '';
-          let currentClass = '';
-          let index = 0;
-          data.forEach(line => {
-            if (line) {
-              let [className, ] = line.split('/');
-              if (currentClass !== className) {
-                currentClass = className;
-                index = 0;
-                plda += '\n';
+
+      let fileWriter = [];
+
+      inputClasses.forEach(cluster => {
+        fs.readFileAsync(`${dependentPath}/${cluster}/data.lst`)
+          .then((data) => {
+            data = data.toString().split('\n');
+            let ndx = '';
+            let plda = '';
+            let trainModel = '';
+            let index = 0;
+            data.forEach(line => {
+              if (line) {
+                let filename = `${cluster}-${pad(index++, 5)}`;
+                ndx += `${filename} ${line}` + '\n';
+                plda += `${filename} `;
+                trainModel += `${filename} ${filename}` + '\n';
               }
-              let filename = `${className}-${pad(index++, 5)}`;
-              ndx += `${filename} ${line}` + '\n';
-              plda += `${filename} `;
-              trainModel += `${filename} ${filename}` + '\n';
-            }
+            });
+            fileWriter.concat([
+              fs.writeFileAsync(`${dependentPath}/${cluster}/Plda.ndx`, plda),
+              fs.writeFileAsync(`${dependentPath}/${cluster}/TrainModel.ndx`,
+                trainModel),
+              fs.writeFileAsync(`${dependentPath}/${cluster}/ivExtractor.ndx`,
+                ndx)
+            ]);
           });
-          fs.writeFileSync(`${ivectorsPath}/Plda.ndx`, plda);
-          fs.writeFileSync(`${ivectorsPath}/TrainModel.ndx`, trainModel);
-          return fs.writeFileAsync(`${extractIVPath}/ivExtractor.ndx`, ndx);
-        })
-        .then(() => {
-          this.set('isIVectorsProcess', false);
-          this.set('isIVectorsDone', true);
-          this.send('extractIVectors');
-        });
+      });
+
+      BluebirdPromise.all(fileWriter).then(() => {
+        this.set('isIVectorsProcess', false);
+        this.set('isIVectorsDone', true);
+        setTimeout(() => this.send('extractIVectors'), 50);
+      });
     },
 
     extractIVectors() {
       console.log('Extract IV');
-      cleanAndRegenerateIV().then(() => {
-        this.set('isExtractProcess', true);
-        let command = `${extractIVPath}/IvExtractor`;
+      this.set('isExtractProcess', true);
+
+      let executeIV = [];
+
+      let command = `${extractIVPath}/IvExtractor`;
+
+      inputClasses.forEach(cluster => {
         let options = [
           `--config ${extractIVPath}/cfg/ivExtractor_fast.cfg`,
           `--featureFilesPath ${prmPath}/`,
           `--labelFilesPath ${lblPath}/`,
-          `--mixtureFilesPath ${gmmPath}/`,
-          `--matrixFilesPath ${matrixPath}/`,
-          `--saveVectorFilesPath ${iv}/raw/`,
-          `--targetIdList ${extractIVPath}/ivExtractor.ndx`
+          `--mixtureFilesPath ${dependentPath}/${cluster}/gmm/`,
+          `--matrixFilesPath ${dependentPath}/${cluster}/mat/`,
+          `--saveVectorFilesPath ${dependentPath}/${cluster}/iv/raw/`,
+          `--targetIdList ${dependentPath}/${cluster}/ivExtractor.ndx`
         ];
 
         let execute = `${command} ${options.join(' ')}`;
-        exec(execute, (error, stdout, stderr) => {
-          if (stderr) {
-            console.log(`stderr: ${stderr}`);
-          }
-          if (error !== null) {
-            console.log(`exec error: ${error}`);
-          }
+        executeIV.push(execAsync(execute));
+      });
+
+      BluebirdPromise.all(executeIV)
+        .then(() => {
           this.set('isExtractProcess', false);
           this.set('isExtractDone', true);
         });
-      });
     }
   }
 });
